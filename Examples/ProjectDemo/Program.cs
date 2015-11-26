90a1f106d4a95447432a22c1c3b8c8ad243efe17
Truls Rødfjell <truls.rodfjell@poweroffice.no>
2015-11-26 12:47:54 +0100

    Updated Documentation/Sdk/GO SDK.chm Updated License.md Added ProjectDemo

---
+++ Examples/ProjectDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using GoApi;
+using GoApi.Core;
+using GoApi.Projects;
+
+namespace ProjectDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                // Set up authorization settings
+                var authorizationSettings = new AuthorizationSettings
+                {
+                    ApplicationKey = "<You Application Key Here>",
+                    ClientKey = "<PowerOffice Go Client Key Here>",
+                    TokenStore = new BasicTokenStore(@"my.tokenstore")
+                };
+
+                // Initialize the PowerOffice Go API and request authorization
+                var api = new Go(authorizationSettings);
+
+                // Create new customer
+                Console.WriteLine("Create new project...");
+                var myNewProject = new Project
+                {
+                    Code = "P105",
+                    Name = "My new project"
+                };
+                api.Project.Save(myNewProject);
+
+                Console.WriteLine("Name: " + myNewProject.Name);
+
+                // Get a list of customers starting with Power (restricted to 50 rows)
+                Console.WriteLine("Getting projects with name starting with \"My\". Max 50...");
+                var customers = api.Project.Get().Where(c => c.Name.ToUpper().StartsWith("MY")).Skip(0).Take(50);
+                foreach (var customer1 in customers)
+                    Console.WriteLine("Name: " + customer1.Name);
+
+                // Look up a project with a given project code
+                Console.WriteLine("Get Project with Code Number: " + myNewProject.Code);
+
+                var project = api.Project.Get().FirstOrDefault(c => c.Code == myNewProject.Code);
+                if (project != null)
+                    Console.WriteLine("Name: " + project.Name);
+
+                // Change project name name
+                project.Name = "My Project";
+
+                // Save the project
+                Console.WriteLine("Saving...");
+                project = api.Project.Save(project);
+                if (project != null)
+                    Console.WriteLine("Name: " + project.Name);
+
+                // Delete the project
+                Console.WriteLine("Deleting...");
+                api.Project.Delete(project);
+
+                Console.WriteLine("Done");
+            }
+            catch (ApiException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            // Wait for user input
+            Console.WriteLine("\n\nPress any key...");
+            Console.ReadKey();
+        }
+    }
+}
--- License.md
+++ License.md
@@ -1,3 +1,21 @@
-At the moment you are not allow to use this software or any of the files in any way.
+The MIT License (MIT)
 
-We will add a less strict license when the SDK is released.
+Copyright (c) 2015 PowerOffice AS
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
