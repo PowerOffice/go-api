db670cfbdf1a85d101939253d1e6cbf9ad275dbf
Truls Rødfjell <truls.rodfjell@poweroffice.no>
2016-09-08 14:34:47 +0200

    Added LedgerReportsAndMatching demo - shows how to get Customer/Supplier open items and statement. And how to match and clear matches.

---
+++ Examples/Reporting/LedgerReportsAndMatching.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoApi;
+using GoApi.Core;
+using GoApi.Reporting.Ledger;
+
+namespace Reporting
+{
+    public class LedgerReportsAndMatching
+    {
+        public static void CustomerLedger()
+        {
+            var authorizationSettings = new AuthorizationSettings
+            {
+                ApplicationKey = "<You Application Key Here>",
+                ClientKey = "<PowerOffice Go Client Key Here>",
+                TokenStore = new BasicTokenStore(@"my.tokenstore")
+            };
+
+            // Initialize the PowerOffice Go API and request authorization
+            var api = new Go(authorizationSettings);
+
+            Console.WriteLine("Customer ledger:");
+            PrintStatement(api);
+            //PrintOpenItems(api);
+
+            //Create a match request with all IDs that you want matched or unmatched.
+            var matchRequest = new MatchRequest(DateTime.Today, new long[] { 2612317, 2890048 }); 
+
+            //Request to clear matches
+            api.Reporting.CustomerLedger.UnmatchEntries(matchRequest);
+            
+            //Request to match entries together
+            api.Reporting.CustomerLedger.MatchEntries(matchRequest);
+
+            //NOTE: The same API methods also exists on api.Reporting.SupplierLedger
+        }
+
+        /// <summary>
+        /// Prints the statement for past 14 days - this includes all customer ledger transactions.
+        /// </summary>
+        /// <param name="api">The API.</param>
+        private static void PrintStatement(Go api)
+        {
+            var customerLedgerLines = api.Reporting.CustomerLedger.GetStatement(DateTime.Today.AddDays(-14), DateTime.Today).ToList();
+            foreach (var customerLedgerEntry in customerLedgerLines)
+            {
+                Console.WriteLine(customerLedgerEntry.Id + " | " + customerLedgerEntry.Customer.Name + "(" +
+                                  customerLedgerEntry.Customer.Code + ")" + " | " + customerLedgerEntry.Amount + " | " +
+                                  customerLedgerEntry.Balance + " | " +
+                                  customerLedgerEntry.PostingDate.ToString("dd.MM.yyyy") + " | " +
+                                  customerLedgerEntry.MatchId);
+            }
+        }
+
+        /// <summary>
+        /// Prints all open items (all transactions on customer ledger that haven't been matched out
+        /// </summary>
+        /// <param name="api">The API.</param>
+        private static void PrintOpenItems(Go api)
+        {
+            var customerLedgerLines = api.Reporting.CustomerLedger.GetOpenItems(DateTime.Today).ToList();
+            foreach (var customerLedgerEntry in customerLedgerLines)
+            {
+                Console.WriteLine(customerLedgerEntry.Id + " | " + customerLedgerEntry.Customer.Name + "(" +
+                                  customerLedgerEntry.Customer.Code + ")" + " | " + customerLedgerEntry.Amount + " | " +
+                                  customerLedgerEntry.Balance + " | " +
+                                  customerLedgerEntry.PostingDate.ToString("dd.MM.yyyy") + " | " +
+                                  customerLedgerEntry.MatchId);
+            }
+        }
+
+    }
+}
